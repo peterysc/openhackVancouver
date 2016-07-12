ed4462cff95b86af7150e4072a5fd6116e416dfe
Andy Tung <andyytung@gmail.com>
2016-07-12 05:00:26 -0700

    cartjs

---
+++ routes/cart.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+var isLoggedIn = function (req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.sendStatus('401');
+    res.end();
+}
+
+router.get('/', isLoggedIn, function (req, res) {
+    console.log(req.user);
+});
+
+module.exports = router;
