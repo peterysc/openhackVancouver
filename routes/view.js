81379abf1b29e35c867d21f95f4b05b778c860fc
maycowmeira <maycowmeira@hotmail.com>
2016-07-12 02:30:20 -0700

    -.-`

--- routes/view.js
+++ routes/view.js
@@ -3,11 +3,15 @@ var passport = require('passport');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.send('index');
+    res.render('index');
 });
 
 router.get('/product', function (req, res, next) {
     res.render('product');
+});
+
+router.get('/signin', function (req, res, next) {
+    res.render('signin');
 });
 
 router.get('/register-user', function (req, res, next) {
