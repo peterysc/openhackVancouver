e7509cce24a18e59ddfd193dd9a57bd893c5206a
maycowmeira <maycowmeira@hotmail.com>
2016-07-12 00:19:47 -0700

    MainPage switching between types and loading gif

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,14 +6,40 @@ function getAllVeggiesProducts(){
   		beforeSend: function (xhr) {
     		xhr.setRequestHeader ("Authorization", "Basic ZjAwNTAxOTlmYjg1YmYwYTkyMGZiMjkwNjk1OTBmYmY5MWQ5Zjk0Zjk1YWJkOWQ2ZGQ6RWdYYWMzQzRSZGhqOUtFQXA1MDZtdkZ1M0t1VXJTaXJHVjYzZDVjcTczVTREeDV4VFY=");
 		},
-  		success:function(data){ 
-  			buildProductPage(data);
+  		success:function(data){
+  			buildHomePage(data);
   		}
 	});
 }
 
-function buildProductPage(data){
+function getAllDairyProducts(){
+	$.ajax({
+  		method: 'GET',
+  		dataType: "json",
+  		url: 'http://192.176.47.48:27030/rest/CatalogManagement/v2/productSpecification/?project=projectOpenFarm',
+  		beforeSend: function (xhr) {
+    		xhr.setRequestHeader ("Authorization", "Basic ZjAwNTAxOTlmYjg1YmYwYTkyMGZiMjkwNjk1OTBmYmY5MWQ5Zjk0Zjk1YWJkOWQ2ZGQ6RWdYYWMzQzRSZGhqOUtFQXA1MDZtdkZ1M0t1VXJTaXJHVjYzZDVjcTczVTREeDV4VFY=");
+		},
+  		success:function(data){ 
+  			buildHomePage(data);
+  		}
+	});
+}
+function getAllFruitsProducts(){
+	$.ajax({
+  		method: 'GET',
+  		dataType: "json",
+  		url: 'http://192.176.47.48:27030/rest/CatalogManagement/v2/productSpecification/?project=projectOpenFarmFruits',
+  		beforeSend: function (xhr) {
+    		xhr.setRequestHeader ("Authorization", "Basic ZjAwNTAxOTlmYjg1YmYwYTkyMGZiMjkwNjk1OTBmYmY5MWQ5Zjk0Zjk1YWJkOWQ2ZGQ6RWdYYWMzQzRSZGhqOUtFQXA1MDZtdkZ1M0t1VXJTaXJHVjYzZDVjcTczVTREeDV4VFY=");
+		},
+  		success:function(data){ 
+  			buildHomePage(data);
+  		}
+	});
+}
 
+function buildHomePage(data){
 	for (var i = 0; i < data.length; i++) {
 		var div = document.createElement("div");
 		div.setAttribute("class", "vegpost");
@@ -31,4 +57,44 @@ function buildProductPage(data){
 		$('#mainBody').append(div);
 	}
 
+	removeLoadingGif();
+}
+
+function clickDairyTab(){
+	$('#mainBody').empty();
+	loadingGif();
+
+	$('#dairyTab').addClass("active");
+	$('#fruitsTab').removeClass("active");
+	$('#veggiesTab').removeClass("active");
+
+	getAllDairyProducts();
+}
+
+function clickFruitsTab(){
+	$('#mainBody').empty();
+	loadingGif();
+
+	$('#fruitsTab').addClass("active");
+	$('#veggiesTab').removeClass("active");
+	$('#dairyTab').removeClass("active");
+	getAllFruitsProducts();
+}
+
+function clickVeggiesTab(){
+	$('#mainBody').empty();
+	loadingGif();
+
+	$('#fruitsTab').removeClass("active");
+	$('#veggiesTab').addClass("active");
+	$('#dairyTab').removeClass("active");
+	getAllVeggiesProducts();
+
+}
+
+function loadingGif(){
+	$('#mainBody').prepend('<img id="loadingGif" src="/images/loading-cow.gif" />');
+}
+function removeLoadingGif(){
+	$('#loadingGif').remove();
 }
